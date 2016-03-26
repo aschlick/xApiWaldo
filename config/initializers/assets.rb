cb41021e296d1f2b4e5dcebf9230ebe2adb38515
Alexander Schlicker <alexander@schlickster.us>
2016-03-26 18:47:54 -0400

    Start screen and actually render picture somehow

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile = %w(application.css)
+if Rails.env.production?
+  Rails.application.config.assets.precompile = %w( application.css )
+else
+  Rails.application.config.assets.precompile += %w( jspm_packages/system.js config.js )
+end
