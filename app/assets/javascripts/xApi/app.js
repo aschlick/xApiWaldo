4296e543ba9949e7669d1bfc56a57c54498ea558
Alexander Schlicker <alexander@schlickster.us>
2016-04-26 22:12:18 -0400

    fixes some problems

--- app/assets/javascripts/xApi/app.js
+++ app/assets/javascripts/xApi/app.js
@@ -3,8 +3,7 @@ export class App{
     config.title = 'Wheres xApi';
     config.map([
       { route: [''], name: 'start', moduleId: 'start', nav: true, title:'Start' },
-      { route: ['pictures'], name: 'pictures', moduleId: 'pictures', nav: false },
-      { route: ['pictures/:id'], name: 'picturesDetail', moduleId: 'pictures', nav: false }
+      { route: ['pictures'], name: 'pictures', moduleId: 'pictures', nav: false }
     ]);
     this.router = router;
   }
